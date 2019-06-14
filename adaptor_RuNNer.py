db956249da8d9549f6bbb282e0cf8f1eec1c00da
Hossein Ghorbanfekr <hgh.comphys@gmail.com>
2019-06-14 22:19:47 +0200

    add VASP adaptor derived class from RuNNer including read_POSCAR() & read_OUTCAR() methods

--- adaptor_RuNNer.py
+++ adaptor_RuNNer.py
@@ -5,6 +5,11 @@ ANGSTROM_TO_BOHR = 1.8897261328
 EV_TO_HARTREE = 0.0367493254
 KCALMOL_TO_HARTREE = 0.001593602
 
+
+def read_and_tokenize_line(in_file):
+    return next(in_file).rstrip("/n").split()
+
+
 class AtomicData:
     """A class that holds atomic data such as positions, forces, total_energy, charges, etc."""
 
@@ -50,6 +55,16 @@ class Sample:
         for atom in self.atomic:
             tot += atom.charge
         return tot
+
+    def find_atoms_with_symbol(self, symbol):
+        sel_atoms = []
+        for atom in self.atoms:
+            if atom.symbol == symbol:
+                sel_atoms.append(atom)
+        return sel_atoms
+
+    def get_number_of_atoms_with_symbol(self, symbol):
+        return len(self.find_atoms_with_symbol(symbol))
 
 
 class DataSet:
@@ -111,10 +126,6 @@ class RunnerAdaptor:
                 out_file.write("charge %.10f\n" % (sample.collective.total_charge*uc.charge))
                 out_file.write("end\n")
         return self
-
-    def read_lammps(self, filename, symbol_dict=None):
-        """A method that reads RuNNer structure file."""
-        pass
 
     def read_nnforces(self, filename, uc=UnitConversion()):
         """A method that reads predicted force for a given structure"""
@@ -196,3 +207,145 @@ class RuNNerAdaptorLAMMPS(RunnerAdaptor):
     def write_lammps(self, filename, uc=UnitConversion()):
         """A method that writes lammps input data."""
         pass
+
+
+class RuNNerAdaptorVASP(RunnerAdaptor):
+    """An inherited class for conversion file formats between RuNNer and VASP packages."""
+
+    def __init__(self):
+        RunnerAdaptor.__init__(self)
+
+    def write_POSCAR(self, filename, uc=UnitConversion()):
+
+        # with open(filename, 'w') as out_file:
+
+            # H2O
+            # 0.52918   ! scaling parameter
+            #  15 0 0
+            #  0 15 0
+            #  0 0 15
+            # 2 1
+            # select
+            # cart
+            #       1.10    -1.43     0.00 T T F
+            #       1.10     1.43     0.00 T T F
+            #       0.00     0.00     0.00 F F F
+
+            # for sample in self.dataset.samples:
+            #
+            #     out_file.write("H2O\n")
+            #     out_file.write("1.00  ! scaling factor\n")
+            #     out_file.write("%.10f %.10f %.10f\n" % (sample.collective.box[0] * uc.length, 0.0, 0.0))
+            #     out_file.write("%.10f %.10f %.10f\n" % (0.0, sample.collective.box[1] * uc.length, 0.0))
+            #     out_file.write("%.10f %.10f %.10f\n" % (0.0, 0.0, sample.collective.box[2] * uc.length))
+            #
+            #     for atom in sample.atomic:
+            #         out_file.write("%15.10f %15.10f %15.10f\n" % tuple([pos*uc.length for pos in atom.position]))
+
+        # It is already implemented in N2P2 :D
+        pass
+
+    def read_POSCAR(self, filename='POSCAR', symbol_list=None, uc=UnitConversion()):
+
+        # create a instance of sample data
+        sample = Sample()
+
+        with open(filename, 'r') as in_file:
+
+            # loop over lines in file
+            for line in in_file:
+
+                # create a instance of sample data
+                sample = Sample()
+
+                # read scaling factor
+                line = read_and_tokenize_line(in_file)
+                scaling_factor = float(line[0])
+                # print (scaling_factor)
+
+                # read box info
+                box = []
+                for n in range(3):
+                    line = read_and_tokenize_line(in_file)
+                    box.append(float(line[n])*scaling_factor*uc.length)
+                # print(box)
+
+                line = read_and_tokenize_line(in_file)
+                natoms_each_type = [int(_) for _ in line]
+                # print (natoms_each_type)
+
+                # skip the line
+                next(in_file)
+
+                # check cartesian coordinates
+                line = next(in_file)
+                if "cart" not in line.lower():
+                    raise AssertionError("Expected cartesian coordinates!")
+
+                # read atomic positions
+                atomid = 0
+                for natoms, n in zip(natoms_each_type, range(len(natoms_each_type))):
+
+                    for i in range(natoms):
+
+                        atomid += 1
+                        line = read_and_tokenize_line(in_file)
+
+                        position = [float(_)*uc.length for _ in line[0:3]]
+                        symbol = symbol_list[n]
+
+                        # create atomic data and append it to sample
+                        sample.atomic.append(AtomicData(atomid, tuple(position), symbol, 0.0, 0.0, (0.0, 0.0, 0.0)))
+                        # (charge, energy, and force) * uc = 0
+                        # print (symbol, position)
+                # Assuming it is the end of the POSCAR
+                break
+
+            # set collective data
+            sample.collective = CollectiveData(tuple(box), 0, 0)  # (total charge & energy) * uc = 0
+
+            # add sample to DataSet (list of samples)
+            self.dataset.append(sample)
+
+        return self
+
+
+    def read_OUTCAR(self, filename='OUTCAR', uc=UnitConversion()):
+
+        with open(filename, 'r') as in_file:
+            # loop over lines in file
+
+            for line in in_file:
+
+                # read line
+                line = next(in_file)
+
+                # read the force section
+                if "POSITION" in line:
+                    next(in_file)
+                    for atom in self.dataset.samples[0].atomic:
+                        line = read_and_tokenize_line(in_file)
+                        force = [float(_)*uc.force for _ in line[3:6]]
+                        atom.force = tuple(force)
+                        # print(line, atom.force)
+
+                if "total energy   ETOTAL" in line:
+                    total_energy = float(line.rstrip("/n").split()[-2])
+                    print (total_energy)
+                    self.dataset.samples[0].collective.total_energy = total_energy
+
+        return self
+
+    def read_vasp(self, symbol_list=None, uc=UnitConversion()):
+        self.read_POSCAR(symbol_list=symbol_list, uc=uc)
+        self.read_OUTCAR(uc=uc)
+
+
+if __name__ == "__main__":
+
+    vasp = RuNNerAdaptorVASP()
+    uc = UnitConversion(energy_conversion=0.1, length_conversion=1)
+    vasp.read_vasp(symbol_list=['O', 'H'], uc=uc)
+    vasp.write_runner(filename='inpu.data.vasp')
+    # vasp.read_POSCAR(symbol_list=['O', 'H'], uc=uc)
+    # vasp.read_OUTCAR(uc=uc)
